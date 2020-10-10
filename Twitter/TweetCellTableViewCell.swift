f4c11b78c194cae734d369a7ccb51c754f69f7c0
Sadia Chowdhury <sadiachowdhury2000@gmail.com>
2020-10-09 23:26:47 -0400

    twitter

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Sadia Chowdhury on 10/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweeterContent: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
